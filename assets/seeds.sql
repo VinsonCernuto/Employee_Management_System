f8012f264ed0a0f2dbec867e52619faebd4a1dc3
Vinson Cernuto <cernutovinson@gmail.com>
2020-10-12 17:09:06 -0400

    finsoshed seeds

---
+++ assets/seeds.sql
@@ -0,0 +1,16 @@
+INSERT INTO departments (dept_name)
+VALUES ("Sales"), ("Web Development"), ("Graphic Design");
+
+INSERT INTO roles (title, salary)
+VALUES 
+("Sales Manager", 75000),
+("Junior Graphic Designer", 45000),
+("Senior Graphic Designer",60000),
+("Junior Web Developer", 60000),
+("Senior Web Developer",85000);
+
+INSERT INTO employees (first_name, last_name)
+values 
+("Vinson", "Cernuto"),
+("John", "Doe")
+
