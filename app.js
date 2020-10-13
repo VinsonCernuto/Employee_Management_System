bdead37b2c6f134bd6baab49e5ace8a8ef64f28a
Vinson Cernuto <cernutovinson@gmail.com>
2020-10-13 17:02:33 -0400

    first questions

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const connection = mysql.createConnection({
     username: 'root',
 
     //Your password
-    password: 'root',
+    password: 'Tank.1996',
     database: 'traker_db'
 });
 
@@ -20,3 +20,26 @@ connection.connect(function(err){
     if (err) throw err;
     console.log("Connected as id " + connection.threadId + "\n");
 });
+
+const questions = [
+    {
+        name: "startingQuestion",
+        type: "list",
+        message: "What Would you like to do?",
+        choices: [
+            "Add departments",
+            "Add roles",
+            "Add employees",
+            "View departments",
+            "View roles",
+            "View employees",
+            "Update employee roles",
+            "Close Program"
+        ]
+    },
+    {
+        name: "departmentName",
+        type: "input",
+        message: "What is the depatment's name?"
+    }
+];
